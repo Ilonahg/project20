07b2696459efd4c84e4aff9c7aafb57efafa3209
ilonahg <gogilchyn2005ilona@gmail.com>
2025-01-08 03:30:22 +0200

    бновлены тесты в test_transactions.py

--- tests/test_transactions.py
+++ tests/test_transactions.py
@@ -1,56 +1,58 @@
 import pytest
+from generators.transactions import (card_number_generator, filter_by_currency, transaction_descriptions)
 
-from generators.transactions import (card_number_generator, filter_by_currency,
-                                     transaction_descriptions)
-
-# Пример данных для тестов
-transactions = [
-    {
-        "id": 939719570,
-        "state": "EXECUTED",
-        "date": "2018-06-30T02:08:58.425572",
-        "operationAmount": {
-            "amount": "9824.07",
-            "currency": {"name": "USD", "code": "USD"}
+# Пример фикстуры, которая генерирует данные транзакций
+@pytest.fixture
+def transactions():
+    return [
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {
+                "amount": "9824.07",
+                "currency": {"name": "USD", "code": "USD"}
+            },
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702"
         },
-        "description": "Перевод организации",
-        "from": "Счет 75106830613657916952",
-        "to": "Счет 11776614605963066702"
-    },
-    {
-        "id": 142264268,
-        "state": "EXECUTED",
-        "date": "2019-04-04T23:20:05.206878",
-        "operationAmount": {
-            "amount": "79114.93",
-            "currency": {"name": "USD", "code": "USD"}
+        {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {
+                "amount": "79114.93",
+                "currency": {"name": "USD", "code": "USD"}
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188"
         },
-        "description": "Перевод со счета на счет",
-        "from": "Счет 19708645243227258542",
-        "to": "Счет 75651667383060284188"
-    },
-    {
-        "id": 873106923,
-        "state": "EXECUTED",
-        "date": "2019-03-23T01:09:46.296404",
-        "operationAmount": {
-            "amount": "43318.34",
-            "currency": {"name": "руб.", "code": "RUB"}
-        },
-        "description": "Перевод со счета на счет",
-        "from": "Счет 44812258784861134719",
-        "to": "Счет 74489636417521191160"
-    }
-]
+        {
+            "id": 873106923,
+            "state": "EXECUTED",
+            "date": "2019-03-23T01:09:46.296404",
+            "operationAmount": {
+                "amount": "43318.34",
+                "currency": {"name": "руб.", "code": "RUB"}
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 44812258784861134719",
+            "to": "Счет 74489636417521191160"
+        }
+    ]
 
 
-def test_filter_by_currency():
+# Используем фикстуру в тестах
+
+def test_filter_by_currency(transactions):
     usd_transactions = filter_by_currency(transactions, "USD")
     result = list(usd_transactions)
     assert len(result) == 2
 
 
-def test_transaction_descriptions():
+def test_transaction_descriptions(transactions):
     descriptions = transaction_descriptions(transactions)
     result = list(descriptions)
     assert result == [
